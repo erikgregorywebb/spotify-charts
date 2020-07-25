b40970793492561fae31ed53f5aba5973b1ff75d
Erik <erikgregorywebb@gmail.com>
2020-07-25 13:28:05 -0400

    Update get-data.R

--- code/get-data.R
+++ code/get-data.R
@@ -7,7 +7,6 @@ dates = seq(as.Date("2017-01-01"), as.Date("2020-07-23"), by="days")
 urls = sprintf('https://spotifycharts.com/regional/global/daily/%s/download', dates)
 
 # compile data
-errors = c()
 datalist = list()
 for (i in 1:length(dates)) {
   Sys.sleep(.5)
@@ -24,17 +23,16 @@ for (i in 1:length(dates)) {
     datalist[[i]] = temp},
     error = function(error_condition) {
       beep(sound = 2) 
-      errors = c(errors, dates[i])
     })
 }
-raw = do.call('rbind', datalist)
+raw = bind_rows(datalist)
+glimpse(raw)
 write_csv(raw, 'spotify-charts-daily-raw.csv')
 
-errors
 
 # clean
 spotify = raw %>%
-  rename(Track = `Track Name`, Date = date) %>%
-  mutate(DOW = weekdays(Date, abbreviate = F))
+  select(-`<html>`) %>%
+  rename(Track = `Track Name`, Date = date)
 
 write_csv(spotify, 'spotify-charts-daily-all-clean.csv')
